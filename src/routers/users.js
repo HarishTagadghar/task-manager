cfc9dce6873c66bb07be6b2d80634a40b163bf3a
Harish Tagadghar <harishtgtg@gmail.com>
2020-04-12 13:08:32 +0530

    handeling error in json instaid of html formet

--- src/routers/users.js
+++ src/routers/users.js
@@ -122,6 +122,8 @@ const upload = multer({
 
 router.post('/user/me/upload' , upload.single('upload') , (req,res) => {
   res.send()
+} , (error,req,res,next) => {
+  res.status(400).send({error: error.message})
 })
 
 module.exports = router;
